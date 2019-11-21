5502edb906d8346b0fdcb5e34319d2c0feef3f0f
sneakwolf777 <peter.d.chung@gmail.com>
2019-11-21 01:20:08 -0800

    finished assignment#5

---
+++ assignment#6/typescript/app.ts
@@ -0,0 +1,7 @@
+import {Puppy} from './puppy';
+
+var hotdog = new Puppy('Édouard');
+console.log(hotdog.bark());
+//  Let's create more puppies
+var oscar = new Puppy('Oscar-Claude');
+console.log(oscar.bark());
